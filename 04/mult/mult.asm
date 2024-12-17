5bc21af1765a324aa822385c95fb9d4fa3bf7588
julianalidya <162549672+julianalidya@users.noreply.github.com>
2024-12-17 16:05:30 +0800

    mult.asm

--- 04/mult/mult.asm
+++ 04/mult/mult.asm
@@ -6,4 +6,37 @@
 // Multiplies R0 and R1 and stores the result in R2.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
-// Put your code here.
+@R2
+M=0
+
+@R0
+D=M
+
+@END
+D;JEQ
+
+@R1
+D=M
+@COUNTER
+M=D
+
+(LOOP)
+@COUNTER
+D=M
+@END
+D;JEQ
+
+@R0
+D=M
+@R2
+M=M+D
+
+@COUNTER
+M=M-1
+
+@LOOP
+0;JMP
+
+(END)
+@END
+0;JMP
