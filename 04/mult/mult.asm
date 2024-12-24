e2868e3c10fc2a10d28cc011e03917564fc38e31
julianalidya <162549672+julianalidya@users.noreply.github.com>
2024-12-24 15:00:14 +0800

    mult.asm

--- 04/mult/mult.asm
+++ 04/mult/mult.asm
@@ -6,37 +6,42 @@
 // Multiplies R0 and R1 and stores the result in R2.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
+// Initialize R2 (result) to 0
 @R2
 M=0
 
+// Load R0 (multiplicand) into D
 @R0
 D=M
 
+// If R0 == 0, skip the loop and leave R2 as 0
 @END
 D;JEQ
 
+// Initialize counter (R1) and start loop
 @R1
-D=M
-@COUNTER
-M=D
-
-(LOOP)
-@COUNTER
-D=M
-@END
-D;JEQ
-
-@R0
-D=M
-@R2
-M=M+D
-
-@COUNTER
-M=M-1
-
+D=M  // Load R1 (multiplier) into D
 @LOOP
-0;JMP
+M=D  // Store R1 (counter) in M
 
+// Start of the loop
+(LOOP)
+    @R1
+    D=M  // Load the counter
+    @END
+    D;JEQ  // If counter == 0, exit loop
+
+    @R0
+    D=M  // Load multiplicand
+    @R2
+    M=M+D  // Add multiplicand to result
+
+    @R1
+    M=M-1  // Decrement counter
+    @LOOP
+    0;JMP  // Repeat loop
+
+// End of the program
 (END)
 @END
 0;JMP
